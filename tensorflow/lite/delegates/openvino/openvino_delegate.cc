d87ca407e272d3447dd70d752cd16e8c33a34d15
Ritul Jasuja <ritul.jasuja@intel.com>
2023-10-24 18:51:04 -0700

    Enable Tensorflow lite OpenVINO Delegate
    Signed-off-by: Ritul Jasuja <ritul.jasuja@intel.com>

---
+++ tensorflow/lite/delegates/openvino/openvino_delegate.cc
@@ -0,0 +1,94 @@
+/* Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "openvino_delegate.h"
+
+#include <ie_cnn_network.h>
+
+#include "openvino/runtime/core.hpp"
+#include "tensorflow/lite/builtin_ops.h"
+#include "tensorflow/lite/delegates/utils/simple_delegate.h"
+
+namespace tflite {
+namespace openvinodelegate {
+class OpenVINODelegate : public SimpleDelegateInterface {
+public:
+    explicit OpenVINODelegate(const TfLiteOpenVINODelegateOptions* options) {
+        options_ = *options;
+        if (options == nullptr) options_ = TfLiteOpenVINODelegateOptionsDefault();
+    }
+
+    bool CheckInputsType(int tensor_id, TfLiteContext* context, TfLiteType expected_type) const {
+        TfLiteType type = context->tensors[tensor_id].type;
+        return expected_type == type;
+    }
+    bool CheckNodeSupportByOpenVINO(const TfLiteRegistration* registration, const TfLiteNode* node,
+                                    TfLiteContext* context) const {
+        switch (registration->builtin_code) {
+            case kTfLiteBuiltinAdd: {
+                int tensor_id1 = node->inputs->data[0];
+                int tensor_id2 = node->inputs->data[1];
+                if (CheckInputsType(tensor_id1, context, kTfLiteFloat32) &&
+                    CheckInputsType(tensor_id2, context, kTfLiteFloat32))
+                    return true;
+                return false;
+            }
+            case kTfLiteBuiltinConv2d: {
+                return true;
+            }
+            default:
+                return false;
+        }
+    }
+
+    bool IsNodeSupportedByDelegate(const TfLiteRegistration* registration, const TfLiteNode* node,
+                                   TfLiteContext* context) const override {
+        bool check = CheckNodeSupportByOpenVINO(registration, node, context);
+        return check;
+    }
+
+    TfLiteStatus Initialize(TfLiteContext* context) override { return kTfLiteOk; }
+
+    const char* Name() const override { return "OpenVINO SimpleDelegate"; }
+
+    std::unique_ptr<SimpleDelegateKernelInterface> CreateDelegateKernelInterface() override {
+        return std::unique_ptr<OpenVINODelegateKernel>(
+            new tflite::openvinodelegate::OpenVINODelegateKernel());
+    }
+
+    SimpleDelegateInterface::Options DelegateOptions() const override {
+        auto options = SimpleDelegateInterface::Options();
+        return options;
+    }
+
+private:
+    TfLiteOpenVINODelegateOptions options_;
+};
+}  // namespace openvinodelegate
+}  // namespace tflite
+
+TfLiteDelegate* TFL_CAPI_EXPORT
+TfLiteCreateOpenVINODelegate(const TfLiteOpenVINODelegateOptions* options) {
+    auto ovdelegate_ = std::make_unique<tflite::openvinodelegate::OpenVINODelegate>(options);
+    return tflite::TfLiteDelegateFactory::CreateSimpleDelegate(std::move(ovdelegate_));
+}
+
+void TFL_CAPI_EXPORT TfLiteDeleteOpenVINODelegate(TfLiteDelegate* delegate) { return; }
+
+TfLiteOpenVINODelegateOptions TFL_CAPI_EXPORT TfLiteOpenVINODelegateOptionsDefault() {
+    TfLiteOpenVINODelegateOptions result;
+    result.debug_level = 0;
+    result.plugins_path = "";
+    return result;
+}
---
+++ tensorflow/lite/delegates/openvino/openvino_delegate_core.cc
@@ -0,0 +1,68 @@
+// openvino_graph_builder.cc
+#include "openvino_delegate_core.h"
+
+namespace tflite {
+namespace openvinodelegate {
+
+TfLiteStatus OpenVINODelegateManager::createGraphfromTfLite(TfLiteContext* context,
+                                                            const TfLiteDelegateParams* params) {
+    const std::unordered_set<int> inputs(&params->input_tensors->data[0],
+                                         &params->input_tensors->data[params->input_tensors->size]);
+    openvino_graph_builder = std::make_unique<OpenVINOGraphBuilder>();
+
+    for (int o = 0; o < params->output_tensors->size; o++) {
+        const int output_tensor_idx = params->output_tensors->data[o];
+        outputs.push_back(output_tensor_idx);
+    }
+
+    for (int i = 0; i < params->nodes_to_replace->size; i++) {
+        const int delegate_node_id = params->nodes_to_replace->data[i];
+        TfLiteNode* delegate_node;
+        TfLiteRegistration* delegate_node_registration;
+        if (context->GetNodeAndRegistration(context, delegate_node_id, &delegate_node,
+                                            &delegate_node_registration))
+            return kTfLiteError;
+
+        for (int k = 0; k < delegate_node->inputs->size; k++) {
+            if (delegate_node_registration->builtin_code == kTfLiteBuiltinTransposeConv && k == 0) {
+                continue;
+            }
+            const int t = delegate_node->inputs->data[k];
+            const void* data = nullptr;
+            if (context->tensors[t].allocation_type == kTfLiteMmapRo) {
+                data = context->tensors[t].data.raw_const;
+                if (openvino_graph_builder->createConstNode(context, t) != kTfLiteOk)
+                    return kTfLiteError;
+            }
+            if (inputs.count(t) != 0) {
+                if (data == nullptr) {
+                    if (openvino_graph_builder->addInputParams(context, t) != kTfLiteOk)
+                        return kTfLiteError;
+                    compute_inputs.push_back(t);
+                }
+            }
+        }
+        if (openvino_graph_builder->createNodeFromTfLiteOp(
+                delegate_node_id, delegate_node_registration, delegate_node, context) != kTfLiteOk)
+            return kTfLiteError;
+    }
+
+    openvino_graph_builder->updateResultNodes(outputs);
+    std::shared_ptr<ov::Model> model = std::make_shared<ov::Model>(
+        openvino_graph_builder->getResultNodes(), openvino_graph_builder->getInputParams());
+    // TODO: get device string from flags
+    std::string deviceStr = "CPU";
+    if (model) {
+        compiled_model = openvino_delegate_core.compile_model(model, deviceStr);
+
+        ov::pass::Manager manager;
+        manager.register_pass<ov::pass::Serialize>("/tmp/model.xml", "/tmp/model.bin");
+        manager.run_passes(model);
+    }
+
+    inferRequest = compiled_model.create_infer_request();
+    return kTfLiteOk;
+}
+
+}  // namespace openvinodelegate
+}  // namespace tflite
