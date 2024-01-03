58afbd3f41ebb1ed9c75746457fadcc1c59a713b
Ritul Jasuja <ritul.jasuja@intel.com>
2024-01-03 17:23:29 +0530

    Update Resize Bilinear and DepthwiseConv2D impl
    Update Graph builder to include transpose layer for input and output
    
    Signed-off-by: Ritul Jasuja <ritul.jasuja@intel.com>

--- tensorflow/lite/delegates/openvino/operations/src/depthwise_conv2d.cc
+++ tensorflow/lite/delegates/openvino/operations/src/depthwise_conv2d.cc
@@ -10,6 +10,7 @@ std::shared_ptr<ov::Node> DepthwiseConv2D::createNode() {
     auto filterNode = getInputNode(tensor_indices[TFLITE_FILTER_NODE]);
     bool has_bias = false;
     ov::Output<ov::Node> biasNode;
+    std::shared_ptr<ov::Node> outputNode;
     std::vector<size_t> strides = {(size_t)depth_conv2dParams->stride_height, (size_t)depth_conv2dParams->stride_width};
     std::vector<size_t> dilations = {(size_t)depth_conv2dParams->dilation_height_factor, (size_t)depth_conv2dParams->dilation_width_factor};
     if (tensor_indices[TFLITE_BIAS_NODE] < 0) {
@@ -34,23 +35,29 @@ std::shared_ptr<ov::Node> DepthwiseConv2D::createNode() {
     }
 
     if (auto_pad == "same-upper") {
-        TfLitePaddingValues paddings = ComputePaddingHeightWidth(strides[0], strides[1], dilations[0], dilations[1], input_dims[1], input_dims[2], filter_dims[1], filter_dims[2], kTfLitePaddingSame, &expected_height, &expected_weight);
-        padding[0] = paddings.height;// top
-        padding[1] = paddings.height + paddings.height_offset; //bottom
-        padding[2] = paddings.width; //left
-        padding[3] = paddings.width + paddings.width_offset; //right
+        padding[0] = 0;// top
+        padding[1] = 0; //bottom
+        padding[2] = 0; //left
+        padding[3] = 0; //right
 	pad_type = ov::op::PadType::SAME_UPPER;
     }
+    //TODO: define paddings for explicit padding
 
-    //TODO: define strides, dilations, padding, auto_pad
     auto depthwise_convNode = std::make_shared<ov::opset3::GroupConvolution>(inputNode, filterNode, ov::Strides(strides), ov::CoordinateDiff(padding[0], padding[2]), ov::CoordinateDiff(padding[1], padding[3]), ov::Strides(dilations), pad_type);
 
-    //TODO: update Bias if needed
     if(has_bias) {
-        auto outputNode = std::make_shared<ov::opset8::Add>(depthwise_convNode, biasNode);
-	return outputNode;
-    } else
-        return depthwise_convNode;
+        auto biasDimensions = GetDims(tensor_indices[TFLITE_BIAS_NODE]);
+        std::vector<uint32_t> shape(depthwise_convNode->get_shape().size(), 1);
+        shape[1] = biasDimensions[0];
+	auto shapeNode = createConstNode(ov::element::i32, ov::Shape{shape.size()}, shape);
+	biasNode = std::make_shared<ov::opset3::Reshape>(biasNode, shapeNode, true);
+	outputNode = std::make_shared<ov::opset3::Add>(
+			depthwise_convNode, biasNode, ov::op::AutoBroadcastType::NUMPY);
+    } else {
+        outputNode = depthwise_convNode;
+    }
+
+    outputNode = ApplyActivation(outputNode, depth_conv2dParams->activation);
 }
 
 }  // namespace openvinodelegate
