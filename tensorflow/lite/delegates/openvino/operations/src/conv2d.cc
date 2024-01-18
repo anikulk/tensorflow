bac491ec7bcac811f04a1282aa33fbe9ada25081
Anisha Kulkarni <anisha.d.kulkarni@intel.com>
2024-01-18 12:45:06 -0800

    Add support for conv2d
    Ensure that resize bilinear doesnt cause -Wc++11-narrowing
    Enable NPU
    
    TEST: conv_test kernel test
    Signed-off-by: Anisha Kulkarni <anisha.d.kulkarni@intel.com>

--- tensorflow/lite/delegates/openvino/operations/src/conv2d.cc
+++ tensorflow/lite/delegates/openvino/operations/src/conv2d.cc
@@ -4,47 +4,54 @@ namespace tflite {
 namespace openvinodelegate {
 
 std::shared_ptr<ov::Node> Conv2D::CreateNode() {
-    const TfLiteConvParams* conv2dParams = (TfLiteConvParams*)GetBuiltinData();
-    std::vector<int> filter_dims = GetDims(tensor_indices_[TFLITE_FILTER_NODE]);
-    std::vector<size_t> strides;
-    std::vector<std::ptrdiff_t> padding_begin, padding_end;
-    std::vector<size_t> dilations;
-    ov::op::PadType auto_pad;
-    int filter_size = 0;
-    int padding_top, padding_bottom, padding_left, padding_right = 0;
+  const TfLiteConvParams* conv2d_params = (TfLiteConvParams*)GetBuiltinData();
+  std::vector<int> filter_dims = GetDims(tensor_indices_[TFLITE_FILTER_NODE]);
+  std::vector<size_t> strides;
+  std::vector<std::ptrdiff_t> padding_begin, padding_end;
+  std::vector<size_t> dilations;
+  ov::op::PadType auto_pad;
+  int filter_size = 0;
+  int padding_top, padding_bottom, padding_left, padding_right = 0;
 
-    if (filter_dims.size() == 4) {
-        filter_size = filter_dims[1];
-    } else {
-        return nullptr;
-    }
-    if (conv2dParams->padding == 0) {
-        TFLITE_LOG(INFO) << "Padding 0\n";
-    } else if (conv2dParams->padding == 1) {
-        auto_pad = ov::op::PadType::EXPLICIT;
+  if (conv2d_params->padding == kTfLitePaddingUnknown) {
+  } else if (conv2d_params->padding == kTfLitePaddingSame) {
+    auto_pad = ov::op::PadType::SAME_UPPER;
+  } else if (conv2d_params->padding == kTfLitePaddingValid) {
+    auto_pad = ov::op::PadType::VALID;
+  }
 
-        padding_top = filter_size / 2;
-        padding_bottom = filter_size / 2;
-        padding_left = filter_size / 2;
-        padding_right = filter_size / 2;
-    } else if (conv2dParams->padding == 2) {
-        auto_pad = ov::op::PadType::VALID;
-        int padding_top, padding_bottom, padding_left, padding_right = 0;
-    }
+  strides = {(size_t)conv2d_params->stride_height,
+             (size_t)conv2d_params->stride_width};
+  padding_begin = {padding_top, padding_left};
+  padding_end = {padding_bottom, padding_right};
+  dilations = {(size_t)conv2d_params->dilation_height_factor,
+               (size_t)conv2d_params->dilation_width_factor};
+  auto input_node = getInputNode(tensor_indices_[TFLITE_INPUT_NODE_1]);
+  auto filter_node = getInputNode(tensor_indices_[TFLITE_FILTER_NODE]);
+  auto bias_node = getInputNode(tensor_indices_[TFLITE_BIAS_NODE]);
+  ov::AxisVector order = {0, 3, 1, 2};
+  const auto order_node =
+        ov::opset3::Constant::create(ov::element::i64, ov::Shape{order.size()}, order);
+  filter_node = std::make_shared<ov::opset3::Transpose>(filter_node, order_node);
 
-    strides = {(size_t)conv2dParams->stride_height, (size_t)conv2dParams->stride_width};
-    padding_begin = {padding_top, padding_left};
-    padding_end = {padding_bottom, padding_right};
-    dilations = {(size_t)conv2dParams->dilation_height_factor,
-                 (size_t)conv2dParams->dilation_width_factor};
-    auto input_node = getInputNode(tensor_indices_[TFLITE_INPUT_NODE_1]);
-    auto filter_node = getInputNode(tensor_indices_[TFLITE_FILTER_NODE]);
-    auto bias_node = getInputNode(tensor_indices_[TFLITE_BIAS_NODE]);
+  auto conv_node = std::make_shared<ov::opset8::Convolution>(
+      input_node, filter_node, ov::Strides(strides),
+      ov::CoordinateDiff(padding_begin), ov::CoordinateDiff(padding_end),
+      ov::Strides(dilations), auto_pad);
+  auto bias_dims = GetDims(tensor_indices_[TFLITE_BIAS_NODE]);
+  std::vector<uint32_t> shape(conv_node->get_shape().size(), 1);
+  shape[1] = bias_dims[0];
+  auto shape_node =
+      CreateConstNode(ov::element::i32, ov::Shape{shape.size()}, shape);
 
-    auto conv_node = std::make_shared<ov::opset8::Convolution>(
-        input_node, filter_node, ov::Strides(strides), ov::CoordinateDiff(padding_begin),
-        ov::CoordinateDiff(padding_end), ov::Strides(dilations), auto_pad);
-    return conv_node;
+  bias_node =
+      std::make_shared<ov::opset3::Reshape>(bias_node, shape_node, true);
+
+  std::shared_ptr<ov::Node> outputNode = std::make_shared<ov::opset3::Add>(
+      conv_node, bias_node, ov::op::AutoBroadcastType::NUMPY);
+
+  return ApplyActivation(outputNode, conv2d_params->activation);
+
 }
 
 }  // namespace openvinodelegate
